3f227bcb88f7b220b5adc28f5519f035e86ee2bf
Gagan <>
2020-06-30 09:29:35 +0530

    removed old code

--- apps/web-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'web-app',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/web-app',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
